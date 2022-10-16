d233dfd3a9fb13ac1a8974188520c0e4520d088a
Shashank Garimella <shashank25225@gmail.com>
2022-10-16 20:18:25 +0530

    updated

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup , find_packages
+
+setup(
+
+    name = "sensor",
+    version="0.01",
+    author="shashank",
+    author_email="shashank25225@gmail.com",
+    packages=find_packages(),
+    install_requires=[],
+)
